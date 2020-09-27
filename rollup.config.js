3bad014bdca7b0844ac2496ee3e39924b45729a1
Graham Kaemmer <graham.kaemmer@gmail.com>
2020-09-27 17:35:49 -0400

    Add plugin-node-resolve

--- rollup.config.js
+++ rollup.config.js
@@ -1,9 +1,10 @@
 import typescript from "@rollup/plugin-typescript";
+import nodeResolve from "@rollup/plugin-node-resolve";
 
 export default {
   input: "src/index.ts",
   output: {
     dir: "out",
   },
-  plugins: [typescript()],
+  plugins: [typescript(), nodeResolve()],
 };
